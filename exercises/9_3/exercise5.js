27100b3c72b473a052e26d248ddb443c3a6aa455
Kaecio <kaecio@yahoo.com.br>
2020-05-13 09:53:29 -0300

    Ecercise 5 done

---
+++ exercises/9_3/exercise5.js
@@ -0,0 +1,14 @@
+const assert = require('assert')
+
+const myList = [1, 2, 3]
+
+// escreva swap abaixo
+const myList = [1,2,3]
+const swap = ([x,y,z]) => [z,y,x]
+const swappedList = swap(myList)
+console.log(myList)
+
+assert.equal(swappedList[0], 3)
+assert.equal(swappedList[1], 2)
+assert.equal(swappedList[2], 1)
+    
